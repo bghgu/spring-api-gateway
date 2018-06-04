b27294cd56590440191916643146a63a8019186a
daseul Bae <ektmf1993@gmail.com>
2018-06-05 00:46:17 +0900

    Add ContentsService

--- src/main/java/tk/bghgu/apigateway/service/ContentsService.java
+++ src/main/java/tk/bghgu/apigateway/service/ContentsService.java
@@ -1,7 +1,14 @@
 package tk.bghgu.apigateway.service;
 
+import tk.bghgu.apigateway.model.Contents;
+
 /**
  * Created by ds on 2018-06-05.
  */
-public class ContentsService {
+public interface ContentsService {
+    void getContentsByContentId(final String content_id);
+    void getContentsByUserId(final String user_id);
+    void postContents(final Contents contents);
+    void putContentsByContentId(final Contents contents, final String content_id);
+    void deleteContentsByContentId(final String content_id);
 }
---
+++ src/main/java/tk/bghgu/apigateway/service/impl/ContentsServiceImpl.java
@@ -0,0 +1,48 @@
+package tk.bghgu.apigateway.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import tk.bghgu.apigateway.model.Contents;
+import tk.bghgu.apigateway.service.ContentsService;
+
+/**
+ * Created by ds on 2018-06-05.
+ */
+
+@Slf4j
+@Service
+public class ContentsServiceImpl implements ContentsService {
+
+
+
+    @Override
+    public void getContentsByContentId(final String content_id) {
+        log.info("getContentsByContentId");
+        log.info(content_id);
+    }
+
+    @Override
+    public void getContentsByUserId(final String user_id) {
+        log.info("getContentsByUserId");
+        log.info(user_id);
+    }
+
+    @Override
+    public void postContents(final Contents contents) {
+        log.info("postContents");
+        log.info(contents.getUser_id());
+    }
+
+    @Override
+    public void putContentsByContentId(final Contents contents, final String content_id) {
+        log.info("putContentsByContentId");
+        log.info(content_id);
+        log.info(contents.getUser_id());
+    }
+
+    @Override
+    public void deleteContentsByContentId(final String content_id) {
+        log.info("deleteContentsByContentId");
+        log.info(content_id);
+    }
+}
